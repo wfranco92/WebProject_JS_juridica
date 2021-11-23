010d8206d5412c25598d19c1277c78033e7f9ff3
wfranco92 <ing.wfranco@gmail.com>
2021-11-23 09:50:22 -0500

    add new function. Implement encrypt password index login

--- js/login.js
+++ js/login.js
@@ -2,15 +2,20 @@ const user=document.getElementById("user");
 const pass=document.getElementById("password");
 const btnSend=document.getElementById("btnsubmit");
 
+var encrypted="U2FsdGVkX19wa4etZupM2KeNrHvRrBkjMVCduXooIuA=";
+var decrypted= CryptoJS.AES.decrypt(encrypted, "EncryptionKey");
+
+
 btnSend.addEventListener("click", (e) =>{
-    if(user.value=="Admin" && pass.value=="Solu2105"){
+    
+    if(user.value=="Admin" && pass.value===decrypted.toString(CryptoJS.enc.Utf8)){
         return true;
     }
     else{
-        
-        //alert("Verifique credenciales de acceso")
+        //alert("Verifique credenciales de acceso")   
         document.getElementById("msgError").style.display="block";
         e.preventDefault();
         return false;
     }
 });
+
